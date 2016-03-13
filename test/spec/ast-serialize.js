40591df78004639ab5af915d401ff1212ba8bdd4
muji <freeformsystems@gmail.com>
2016-03-13 11:25:17 +0800

    Compare XML render in test spec.

--- test/spec/ast-serialize.js
+++ test/spec/ast-serialize.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect
   , cmark = require('commonmark')
   , Parser = cmark.Parser
+  , Renderer = cmark.XmlRenderer
   , ast = require('../../index')
   , Deserialize = require('../../lib/deserialize');
 
@@ -9,12 +10,13 @@ describe('mkast:', function() {
   it('should parse string input w/ callback', function(done) {
     var parser = new Parser()
       , buffer = parser.parse('# Title')
+      , expected = (new Renderer()).render(buffer)
       , stream = ast.serialize(buffer)
       , deserializer = new Deserialize();
 
     function complete(doc) {
       expect(doc).to.be.an('object');
-      expect(doc).to.eql(buffer);
+      expect((new Renderer()).render(doc)).to.eql(expected);
       done(); 
     }
 
