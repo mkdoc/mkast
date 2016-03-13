cf5dbb50a61d041d796a4bd5b35a692f59450c9e
muji <freeformsystems@gmail.com>
2016-03-13 15:07:34 +0800

    Complete initial test specs.

--- test/spec/ast-serialize.js
+++ test/spec/ast-serialize.js
@@ -3,16 +3,15 @@ var expect = require('chai').expect
   , Parser = cmark.Parser
   , Renderer = cmark.XmlRenderer
   , ast = require('../../index')
+  , Walk = require('../../lib/walk')
   , Deserialize = require('../../lib/deserialize');
 
 describe('mkast:', function() {
 
   it('should serialize ast w/ callback', function(done) {
-
     var parser = new Parser()
-      , buffer = parser.parse('# Title\n<? @include file.md ?>')
-      , expected = (new Renderer()).render(buffer)
-      , stream = ast.serialize(buffer, done);
+      , buffer = parser.parse('# Title\n<? @include file.md ?>');
+    ast.serialize(buffer, done);
   });
 
 
@@ -32,6 +31,24 @@ describe('mkast:', function() {
 
     stream.pipe(deserializer);
     deserializer.once('eof', complete);
+  });
+
+  it('should skip non-document on walk', function(done) {
+    var walker = new Walk();
+    walker.once('finish', done);
+    walker.end({_type: 'foo'});
+  });
+
+  it('should error on bad json', function(done) {
+    var deserializer = new Deserialize();
+    deserializer.once('error', function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(Error);
+      done(); 
+    });
+    deserializer.end(new Buffer('{'));
   });
 
 });
