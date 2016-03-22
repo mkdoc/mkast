bded3ec2f91d989a46d32033bb0794b73c2b7c6c
muji <freeformsystems@gmail.com>
2016-03-22 10:35:41 +0800

    Initial serialize assertion.

---
+++ test/spec/serialize.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect
+  , ast = require('../../index')
+  , Node = require('../../lib/node');
+
+describe('mkast:', function() {
+
+  it('should serialize paragraph', function(done) {
+    var doc = ast.parse('Paragraph\n\n')
+      , obj = Node.serialize(doc);
+    expect(doc).to.be.an('object');
+    expect(obj).to.be.an('object');
+    expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
+    expect(Node.is(obj.firstChild, Node.PARAGRAPH)).to.eql(true);
+    expect(Node.is(obj.firstChild.firstChild, Node.TEXT)).to.eql(true);
+    expect(obj.firstChild.firstChild.literal).to.eql('Paragraph');
+    done();
+  });
+
+});
