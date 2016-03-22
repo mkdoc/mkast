4967589aac6bb7780077310db6ae4dea8e58cf4f
muji <freeformsystems@gmail.com>
2016-03-22 12:50:18 +0800

    Improve node serialize test specs.

--- test/spec/serialize.js
+++ test/spec/serialize.js
@@ -70,6 +70,25 @@ describe('mkast:', function() {
     done();
   });
 
+  it('should serialize onEnter and onExit', function(done) {
+    var doc = ast.parse('Text')
+      , enter = 'enter'
+      , exit = 'exit'
+      , obj;
+
+    doc._onEnter = enter;
+    doc._onExit = exit;
+    obj = Node.serialize(doc);
+
+    expect(doc).to.be.an('object');
+    expect(obj).to.be.an('object');
+    expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
+    expect(obj.onEnter).to.eql(enter);
+    expect(obj.onExit).to.eql(exit);
+    done();
+  });
+
+
   it('should serialize link references', function(done) {
     var doc = ast.parse('[example]: http://example.com "Example Website"')
       , obj = Node.serialize(doc);
@@ -119,6 +138,60 @@ describe('mkast:', function() {
     expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
     expect(Node.is(obj.firstChild, Node.CODE_BLOCK)).to.eql(true);
     expect(obj.firstChild.literal).to.eql('var foo="bar";\n');
+    done();
+  });
+
+  it('should serialize link', function(done) {
+    var doc = ast.parse('[example](http://example.com "Example Website")')
+      , obj = Node.serialize(doc);
+
+    expect(doc).to.be.an('object');
+    expect(obj).to.be.an('object');
+    expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
+    expect(Node.is(obj.firstChild, Node.PARAGRAPH)).to.eql(true);
+    expect(Node.is(obj.firstChild.firstChild, Node.LINK)).to.eql(true);
+    expect(obj.firstChild.firstChild.destination).to.eql('http://example.com');
+    expect(obj.firstChild.firstChild.title).to.eql('Example Website');
+    expect(Node.is(obj.firstChild.firstChild.firstChild, Node.TEXT))
+      .to.eql(true);
+    expect(obj.firstChild.firstChild.firstChild.literal).to.eql('example');
+    done();
+  });
+
+  it('should serialize link with _linkType', function(done) {
+    var doc = ast.parse('[example](http://example.com)')
+      , obj;
+
+    expect(doc).to.be.an('object');
+    doc.firstChild.firstChild._linkType = 'ref';
+
+    obj = Node.serialize(doc);
+
+    expect(obj).to.be.an('object');
+    expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
+    expect(Node.is(obj.firstChild, Node.PARAGRAPH)).to.eql(true);
+    expect(Node.is(obj.firstChild.firstChild, Node.LINK)).to.eql(true);
+    expect(obj.firstChild.firstChild.linkType).to.eql('ref');
+    done();
+  });
+
+  it('should serialize list', function(done) {
+    var doc = ast.parse('* foo\n* bar\n')
+      , obj = Node.serialize(doc);
+
+    expect(doc).to.be.an('object');
+    expect(obj).to.be.an('object');
+    expect(Node.is(obj, Node.DOCUMENT)).to.eql(true);
+    expect(Node.is(obj.firstChild, Node.LIST)).to.eql(true);
+    expect(Node.is(obj.firstChild.firstChild, Node.ITEM)).to.eql(true);
+
+    // NOTE: list item has paragraph
+    expect(
+      obj.firstChild.firstChild.firstChild.firstChild.literal).to.eql('foo');
+    // NOTE: list item has paragraph
+    expect(
+        obj.firstChild.firstChild.next.firstChild.firstChild.literal
+      ).to.eql('bar');
     done();
   });
 
